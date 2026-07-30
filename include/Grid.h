9bbeb0f92a7378e14559efe0f02b7c6ec6c30811
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 18:00:06 -0500

    add default ctor and matrix std vector

--- include/Grid.h
+++ include/Grid.h
@@ -1,6 +1,11 @@
 #include "Tile.h"
+#include <vector>
 
 class Grid {
 public:
+  Grid();
+  [[nodiscard]] auto get_matrix() const -> const std::vector<Tile> &;
+
 private:
+  std::vector<Tile> m_matrix;
 };
