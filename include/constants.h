f02010f3a51ab5d0201439d470854f6403861af2
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 12:47:11 -0500

    init

---
+++ include/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace constants {
+
+inline constexpr int exit_code_success = 0;
+
+}  // namespace constants
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "constants.h"
+
+int main() {
+  std::cout << "Hello, Minesweeper!\n";
+  return constants::exit_code_success;
+}
