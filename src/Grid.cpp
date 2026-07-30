2db305122159e3d11d8db204be3387f3384fa377
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 18:48:29 -0500

    grid default ctor

---
+++ src/Grid.cpp
@@ -0,0 +1,8 @@
+#include "Grid.h"
+#include "constants.h"
+#include <algorithm>
+
+Grid::Grid()
+    : m_matrix(constants::number_of_tiles),
+      m_active_bombs(
+          static_cast<int>(std::ranges::count_if(m_matrix, &Tile::has_bomb))) {}
