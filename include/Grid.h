8d3a956af36015f2eac3dd56dd89176e86a7dc0d
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 18:17:50 -0500

    add more methods to Grid class

--- include/Grid.h
+++ include/Grid.h
@@ -5,7 +5,9 @@ class Grid {
 public:
   Grid();
   [[nodiscard]] auto get_matrix() const -> const std::vector<Tile> &;
+  [[nodiscard]] auto get_neighbors_of(const Tile &tile) -> std::vector<Tile>;
 
 private:
   std::vector<Tile> m_matrix;
+  int m_active_bombs;
 };
