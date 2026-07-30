72817e31a56732b03409bf2cf3c2021fc7c8c84f
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 18:44:14 -0500

    update constants for num tiles

--- include/constants.h
+++ include/constants.h
@@ -2,6 +2,7 @@
 
 namespace constants {
 
-inline constexpr int exit_code_success = 0;
+inline constexpr int exit_code_success{0};
+inline constexpr int number_of_tiles{9};
 
-}  // namespace constants
+} // namespace constants
