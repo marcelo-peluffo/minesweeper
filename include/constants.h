3759bf3150e3e7604953eed791ad321b0e74fb24
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 19:14:56 -0500

    cleaned up constants namespace and auto

--- include/constants.h
+++ include/constants.h
@@ -2,7 +2,8 @@
 
 namespace constants {
 
-inline constexpr int exit_code_success{0};
-inline constexpr int number_of_tiles{9};
+inline constexpr auto exit_code_success{0};
+inline constexpr auto number_of_tiles{9};
+inline constexpr auto percent_chance_for_bomb{25};
 
 } // namespace constants
