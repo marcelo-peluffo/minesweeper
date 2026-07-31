559d6e7ad8be538ad27033102c382305f926bdf8
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-07-30 19:08:09 -0500

    add matrix getter

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -6,3 +6,5 @@ Grid::Grid()
     : m_matrix(constants::number_of_tiles),
       m_active_bombs(
           static_cast<int>(std::ranges::count_if(m_matrix, &Tile::has_bomb))) {}
+
+auto Grid::get_matrix() const -> const std::vector<Tile> & { return m_matrix; }
