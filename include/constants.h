eacc896288b97aacfc805a6ffad7d946968e12ef
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-08-01 17:11:53 -0500

    correctly calculate # of tiles

--- include/constants.h
+++ include/constants.h
@@ -3,9 +3,9 @@
 namespace constants {
 
 inline constexpr auto exit_code_success{0};
-inline constexpr auto number_of_tiles{9};
 inline constexpr auto number_of_tiles_per_row{10};
 inline constexpr auto number_of_rows{8};
+inline constexpr auto number_of_tiles{number_of_rows * number_of_tiles_per_row};
 inline constexpr auto percent_chance_for_bomb{25};
 
 } // namespace constants
