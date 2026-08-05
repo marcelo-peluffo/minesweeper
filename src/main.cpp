392f07119db1e26ea7f3a480ff28a263ec1370f9
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-08-05 10:00:32 -0500

    add render in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,38 @@
 #include "constants.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/VideoMode.hpp>
 
+struct Tile {
+  bool has_bomb_{};
+  int neighboring_bombs_{};
+};
+
+struct GameState {
+  std::vector<Tile> grid_;
+  bool game_over_;
+};
+
+auto render(sf::RenderWindow &window, const GameState &state) -> void;
+
 int main() {
+  GameState game_state{};
   sf::RenderWindow window(sf::VideoMode({constants::width, constants::height}),
                           "Minesweeper");
 
-  while (window.isOpen()) {
-    while (auto event{window.pollEvent()}) {
-      if (event->is<sf::Event::Closed>()) {
-        window.close();
-      }
-
-      window.clear(sf::Color::White);
-      // draw here
-
-      window.display();
-    }
+  while (!game_state.game_over_) {
+    render(window, game_state);
   }
 
+  window.close();
+
   return 0;
+}
+
+auto render(sf::RenderWindow &window, const GameState &state) -> void {
+
+  window.clear(sf::Color::White);
+  // draw here
+
+  window.display();
 }
