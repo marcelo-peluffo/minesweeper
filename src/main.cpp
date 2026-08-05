a387788ae980dbaff7701fc6ab04b87ba8189bdb
marcelo-peluffo <198099507+marcelo-peluffo@users.noreply.github.com>
2026-08-05 09:50:56 -0500

    add main after nukeK

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,23 @@
+#include "constants.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
 
-int main() { return 0; }
+int main() {
+  sf::RenderWindow window(sf::VideoMode({constants::width, constants::height}),
+                          "Minesweeper");
+
+  while (window.isOpen()) {
+    while (auto event{window.pollEvent()}) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+
+      window.clear(sf::Color::White);
+      // draw here
+
+      window.display();
+    }
+  }
+
+  return 0;
+}
